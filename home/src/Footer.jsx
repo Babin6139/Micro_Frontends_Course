e63ec5e2df51995186a6ad86e42712a594ae0494
Babin6139 <khatribabin39@gmail.com>
2023-03-05 11:08:40 +0545

    feat: created header and footer in home

---
+++ home/src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Footer(){
+    return <div className="p-5 bg-blue-300 text-3xl text-white font-bold ">
+        Only the best fidget spinners in the World
+    </div>
+}
